8148c7da8febbe4fb301ca8a2a5ba5fb901b9c5f
zverkovnikita09 <zverkovnikita09@gmail.com>
2022-07-13 19:39:09 +0300

    making mobile version

--- js/general_script.js
+++ js/general_script.js
@@ -5,7 +5,6 @@ const burger_menu = document.querySelector('#burger_menu');
 const burger_nav = document.querySelector('#burger_nav');
 const modal = document.querySelector('#modal');
 const overlay = document.querySelector('#overlay');
-const approve_form = '<div class="form__approve"><p>Спасибо за обращение</p><p>Наши консультанты свяжутся с Вами в ближайшее время</p><button type="button" class="button approve__button">Закрыть</button></div>';
 
 
 burger.addEventListener('click',()=>{
@@ -43,11 +42,15 @@ window.addEventListener('resize', function(){
 
 window.addEventListener('click', (e)=>{
     if(e.target==overlay){
+        CloseModal();
+    }
+})
+
+function CloseModal(){
         modal.classList.remove('active');
         overlay.style.display = 'none';
         body.style.overflowY = 'auto';
-    }
-})
+}
 
 function ShowModal(){
         overlay.style.display = 'block';
@@ -59,11 +62,14 @@ function ShowModal(){
         body.style.overflowY = 'hidden';
 }
 
-const form_buttons = document.querySelectorAll('.form_button');
-const forms = document.forms;
+const modal_button = document.querySelector('.modal_button');
 
-for(let i=0; i<form_buttons.length; i++){
-    form_buttons[i].addEventListener('click', ()=>{
-        forms[i].innerHTML = approve_form;
-    })
+modal_button.onclick = () =>{
+    let width = modal.clientWidth + 'px';
+    let height = modal.clientHeight +'px';
+    console.log(width)
+    console.log(height)
+    modal.innerHTML = '<div class="form__approve"><h2>Спасибо за обращение!</h2><p>В ближайшее время мы вам позвоним, чтобы провести консультацию. Пожалуйста, подождите звонка нашего консультанта.</p><button type="button" onclick="CloseModal();" class="approve__button modal_button">Закрыть</button></div>';
+    modal.style.width = width;
+    modal.style.height = height;
 }
